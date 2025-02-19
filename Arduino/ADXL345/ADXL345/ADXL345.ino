ce5972c4b170760a76f26c2639e1b4237dcb7383
lusiess01 <lukas.siess@gmail.com>
2025-02-19 21:32:25 +0100

    add gravity compensation

--- Arduino/ADXL345/ADXL345/ADXL345.ino
+++ Arduino/ADXL345/ADXL345/ADXL345.ino
@@ -3,14 +3,17 @@
 
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-const int motorPin = 9;
+const int motorPin = 9;  // PWM-Pin für den Motor
 const int pwmDuty = 128; // 50% Duty Cycle
 
-float gravityOffset = 0; // Kalibrierung der Erdbeschleunigung
+float accelOffsetX = 0; // Kalibrierung für X-Achse
+float accelOffsetY = 0; // Kalibrierung für Y-Achse
+float accelOffsetZ = 0; // Kalibrierung für Z-Achse
 
 void setup() {
     Serial.begin(115200);
     pinMode(motorPin, OUTPUT);
+    analogWrite(motorPin, 0); // Motor zunächst deaktivieren
 
     // ADXL345 Initialisieren
     if (!accel.begin()) {
@@ -20,21 +23,31 @@ void setup() {
 
     accel.setRange(ADXL345_RANGE_16_G); // Höchste Empfindlichkeit
 
-    // Kalibrierung der Erdbeschleunigung
-    Serial.println("Kalibriere Erdbeschleunigung...");
-    float sum = 0;
-    for (int i = 0; i < 100; i++) {
+    // **Kalibrierung des Sensors in der Ruhelage**
+    Serial.println("Kalibriere ADXL345 in der Ruhelage...");
+    float sumX = 0, sumY = 0, sumZ = 0;
+
+    for (int i = 0; i < 100; i++) {  // 100 Messwerte aufnehmen
         sensors_event_t event;
         accel.getEvent(&event);
-        sum += event.acceleration.z;
+        sumX += event.acceleration.x;
+        sumY += event.acceleration.y;
+        sumZ += event.acceleration.z;
         delay(10);
     }
-    gravityOffset = sum / 100.0; // Durchschnittliche statische Z-Beschleunigung
 
-    Serial.print("Erdbeschleunigung (Offset): ");
-    Serial.println(gravityOffset);
+    // Mittelwert als Offset berechnen
+    accelOffsetX = sumX / 100.0;
+    accelOffsetY = sumY / 100.0;
+    accelOffsetZ = sumZ / 100.0;
 
-    // Motor auf 40 Hz setzen
+    Serial.print("Offset X: "); Serial.println(accelOffsetX);
+    Serial.print("Offset Y: "); Serial.println(accelOffsetY);
+    Serial.print("Offset Z: "); Serial.println(accelOffsetZ);
+    Serial.println("Kalibrierung abgeschlossen.");
+
+    // **Jetzt den Motor aktivieren**
+    Serial.println("Starte Motor...");
     analogWrite(motorPin, pwmDuty);
 }
 
@@ -43,15 +56,18 @@ void loop() {
     accel.getEvent(&event);
 
     unsigned long currentTime = millis();
-    
-    // Dynamische Beschleunigung = gemessene Beschleunigung - Erdbeschleunigung
-    float dynamicAcceleration = event.acceleration.z - gravityOffset;
 
+    // **Offset-Korrektur der Messwerte**
+    float correctedX = event.acceleration.x - accelOffsetX;
+    float correctedY = event.acceleration.y - accelOffsetY;
+    float correctedZ = event.acceleration.z - accelOffsetZ;
+
+    // **Nur die Z-Achse zur Vibrationsanalyse**
     Serial.print(currentTime);
     Serial.print(",");
-    Serial.print(40); // Konstante Frequenz
+    Serial.print(40); // Konstante Frequenz (40 Hz)
     Serial.print(",");
-    Serial.println(dynamicAcceleration);
+    Serial.println(correctedZ);
 
-    delay(100); // Messung alle 100 ms
+    delay(10); // Kürzere Wartezeit für mehr Messpunkte
 }
