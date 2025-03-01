c828457d571f365f006789ea7f3f7eb60bdcef0e
lusiess01 <lukas.siess@gmail.com>
2025-03-01 16:49:40 +0100

    mixed Domain Model

--- ModelVoiceCoil/init.m
+++ ModelVoiceCoil/init.m
@@ -2,4 +2,17 @@ clear all
 close all
 clc
 
-%%
+%% Parameter
+
+l=100;
+Re=0.8502*(l/1000); %coil resistance 34AWG ==> 0.160mm ==> 0.020106mm2 ==> 0.8502Ohm/km
+Le=1e-6;  %coil inductance
+
+m = 0.005;      % Masse [kg]
+c = 0.7840;      % Dämpfung [Ns/m]
+k = 100;    % Federkonstante [N/m]
+
+% Elektrische Parameter
+mm = m;      % Induktivität [H]
+rm = c;      % Widerstand [Ohm]
+cm = 1/k;    % Kapazität [F]
