510e3dfe1142fddf2db158b650a8b289567b1a73
lusiess01 <lukas.siess@gmail.com>
2025-06-05 10:08:09 +0200

    some testings

--- ModelVoiceCoil/Matlab/init.m
+++ ModelVoiceCoil/Matlab/init.m
@@ -6,17 +6,17 @@ clc
 
 f =40; % Hz
 rho = 1.68e-8;  % spezifischer Widerstand für Kupfer (Ω·m)
-l = 9.57;       % berechnete Drahtlänge (m)
-d = 0.00016;    % Durchmesser des Drahtes (m)
+l = 5.672;       % berechnete Drahtlänge (m)
+d = 0.0001;    % Durchmesser des Drahtes (m)
 
 % Querschnittsfläche des Drahtes
 A = (pi * (d^2)) / 4;
 
 Re = (rho * l) / A;
-Le=1e-9; 
+Le=181e-6; 
 
 m = (0.3+0.1+0.4+3.6)/1000;      % Masse [kg]
-c = 0;      % Dämpfung [Ns/m]
+c = 1;      % Dämpfung [Ns/m]
 k = 3.775177608796545*1000;    % Federkonstante [N/m]
 
 % Elektrische Parameter
